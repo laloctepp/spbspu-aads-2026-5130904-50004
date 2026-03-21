ddf684b1327ff95558959ba3ae13c7c5b4bd9e1d
laloctepp <lbychkovskaya.omsk@gmail.com>
2026-03-21 14:23:39 +0300

    fix base

--- bychkovskaya.elizaveta/S1/bilist.hpp
+++ bychkovskaya.elizaveta/S1/bilist.hpp
@@ -1,36 +1,32 @@
 template< class T > struct BiList {
 	T val;
 	BiList<T>* next;
-	BiList<T>* prev;
 };
 
 template< class T >
 BiList<T>* new_fake(void) {
 	BiList<T>* r = static_cast<BiList<T>*>(::operator new (sizeof(BiList<T>)));
 	r->next = r;
-	r->prev = r;
 	return r;
 }
 
 template< class T >
-BiList<T>* add(BiList<T>* f, const T& d) {   //добавить перед головой (в начало)
-	BiList<T>* n = new BiList<T>{ d, f->next, f };
-	f->next->prev = n;
+BiList<T>* insert_after(BiList<T>* f, const T& d) {   //добавить после
+	BiList<T>* n = new BiList<T>{ d, f->next };
 	f->next = n;
-	return n;
+	return n;   //возвращаем указатель на добавленный элемент
 }
 
 template< class T >
-BiList<T>* cut(BiList<T>* f) noexcept { 	//удалить голову 
+BiList<T>* erase_after(BiList<T>* f) noexcept { 	//удалить после 
 	if (f->next == f) {
 		return f;
 	}
 	BiList<T>* h = f->next;
 	BiList<T>* subh = f->next->next;
 	f->next = subh;
-	subh->prev = f;
 	delete h;
-	return subh;   //возвращаем указатель на новую голову
+	return subh;   //возвращаем указатель на элемент после которого удалили
 }
 
 template< class T >
@@ -42,7 +38,6 @@ BiList<T>* clear(BiList<T>* fake) noexcept {     //очистить память
 		curr = n;
 	}
 	fake->next = fake;
-	fake->prev = fake;
 	return fake;
 }
 
