8e491d886c9adda6c8258dbbb6d6b5b35840dc93
laloctepp <lbychkovskaya.omsk@gmail.com>
2026-03-22 19:15:56 +0300

    fix list to node

--- bychkovskaya.elizaveta/S1/list.hpp
+++ bychkovskaya.elizaveta/S1/list.hpp
@@ -1,39 +1,39 @@
-template< class T > struct List {
+template< class T > struct Node{
 	T val;
-	List<T>* next;
+	Node<T>* next;
 };
 
 template< class T >
-List<T>* new_fake(void) {
-	List<T>* r = static_cast<List<T>*>(::operator new (sizeof(List<T>)));
+Node<T>* new_fake(void) {
+	Node<T>* r = static_cast<Node<T>*>(::operator new (sizeof(Node<T>)));
 	r->next = r;
 	return r;
 }
 
 template< class T >
-List<T>* insert_after(List<T>* f, const T& d) {   //добавить после
-	List<T>* n = new List<T>{ d, f->next };
+Node<T>* insert_after(Node<T>* f, const T& d) {   //добавить после
+	Node<T>* n = new Node<T>{ d, f->next };
 	f->next = n;
 	return n;   //возвращаем указатель на добавленный элемент
 }
 
 template< class T >
-List<T>* erase_after(List<T>* f) noexcept { 	//удалить после 
+Node<T>* erase_after(Node<T>* f) noexcept { 	//удалить после 
 	if (f->next == f) {
 		return f;
 	}
-	List<T>* h = f->next;
-	List<T>* subh = f->next->next;
+	Node<T>* h = f->next;
+	Node<T>* subh = f->next->next;
 	f->next = subh;
 	delete h;
 	return subh;   //возвращаем указатель на элемент до которого удалили
 }
 
 template< class T >
-List<T>* clear(List<T>* fake) noexcept {     //очистить память
-	List<T>* curr = fake->next;
+Node<T>* clear(Node<T>* fake) noexcept {     //очистить память
+	Node<T>* curr = fake->next;
 	while (curr != fake) {
-		List<T>* n = curr->next;
+		Node<T>* n = curr->next;
 		delete curr;
 		curr = n;
 	}
@@ -42,21 +42,21 @@ List<T>* clear(List<T>* fake) noexcept {     //очистить память
 }
 
 template< class T >
-List<T>* copy(const List<T>* fake) {
-	List<T>* curr = fake->next;
-	List<T>* cpfake = new_fake();   //если исключение то все ок
-	List<T>* prev = cpfake;
+Node<T>* copy(const Node<T>* fake) {
+	Node<T>* curr = fake->next;
+	Node<T>* cpfake = new_fake();   //если исключение то все ок
+	Node<T>* prev = cpfake;
 	while (curr != fake) {
 		try {
-			List<T>* n = new List<T>{ curr->val, cpfake };
+			Node<T>* n = new Node<T>{ curr->val, cpfake };
 			prev->next = n;
 			prev = n;
 			curr = curr->next;
 		}
 		catch (const std::bad_alloc&) {
-			List<T>* cpcurr = cpfake->next;
+			Node<T>* cpcurr = cpfake->next;
 			while (cpcurr != cpfake) {
-				List<T>* temp = cpcurr->next;
+				Node<T>* temp = cpcurr->next;
 				delete cpcurr;
 				cpcurr = temp;
 			}
