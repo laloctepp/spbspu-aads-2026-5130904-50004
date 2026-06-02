f0505c856435c3a9f099a1e5b33b303158bf3d38
laloctepp <lbychkovskaya.omsk@gmail.com>
2026-06-02 15:50:14 +0300

    add translation units, iter methods

--- bychkovskaya.elizaveta/S1/list.hpp
+++ bychkovskaya.elizaveta/S1/list.hpp
@@ -1,3 +1,8 @@
+#ifndef LIST_HPP
+#define LIST_HPP
+
+#include "iters.hpp"
+
 template< class T > struct Node{
 	T val;
 	Node<T>* next;
@@ -58,6 +63,9 @@ template< class T > struct List {
 	T& front();
 	const T& front() const;
 	void push_front(const T& d);
+
+	LIter<T> insertAfter(LIter<T> pos, const T& val);
+    LIter<T> eraseAfter(LIter<T> pos);
 };
 
 template< class T >
@@ -138,111 +146,16 @@ List<T>& List<T>::operator=(List<T> other) {
     return *this;
 }
 
-template < class T >
-struct LIter {
-private:
-	friend class List<T>;
-	Node<T>* node;
-
-public:
-	LIter() : node(nullptr) {};
-	LIter(Node<T>* n) : node(n) {};
-	LIter< T >& operator++();
-	LIter< T > operator++(int);
-	bool operator!=(LIter< T >) const;
-	bool operator==(LIter< T >) const;
-	T& operator*() const;
-	T* operator->() const;
-};
-
 template< class T >
-LIter< T >& LIter< T >::operator++() {
-	assert(node != nullptr);
-	node = node->next;
-	return *this;
+LIter<T> List<T>::insertAfter(LIter<T> pos, const T& val) {
+    Node<T>* newNode = insert_after(pos.node, val);
+    return LIter<T>(newNode);
 }
 
 template< class T >
-LIter< T > LIter< T >::operator++(int) {
-	assert(node != nullptr);
-	LIter< T > result(*this);
-	++(*this);
-	return result;
+LIter<T> List<T>::eraseAfter(LIter<T> pos) {
+    Node<T>* nextNode = erase_after(pos.node);
+    return LIter<T>(nextNode);
 }
 
-template< class T >
-bool LIter< T >::operator==(LIter< T > rhs) const {
-	return node == rhs.node;
-}
-
-template< class T >
-bool LIter< T >::operator!=(LIter< T > rhs) const {
-	return !(rhs == *this);
-}
-
-template< class T >
-T& LIter< T >::operator*() const {
-	assert(node != nullptr);
-	return node->val;
-}
-template< class T >
-T* LIter< T >::operator->() const {
-	assert(node != nullptr);
-	return std::addressof(node->val);
-}
-
-template < class T >
-struct LCIter {
-private:
-	friend class List<T>;
-	const Node<T>* node;
-
-public:
-	LCIter(const LIter<T>& other) : node(other.node) {}
-
-	LCIter() : node(nullptr) {};
-	LCIter(const Node<T>* n) : node(n) {};
-	LCIter< T >& operator++();
-	LCIter< T > operator++(int);
-	bool operator!=(LCIter< T >) const;
-	bool operator==(LCIter< T >) const;
-	const T& operator*() const;
-	const T* operator->() const;
-};
-
-template< class T >
-LCIter< T >& LCIter< T >::operator++() {
-	assert(node != nullptr);
-	node = node->next;
-	return *this;
-}
-
-template< class T >
-LCIter< T > LCIter< T >::operator++(int) {
-	assert(node != nullptr);
-	LCIter< T > result(*this);
-	++(*this);
-	return result;
-}
-
-template< class T >
-bool LCIter< T >::operator==(LCIter< T > rhs) const {
-	return node == rhs.node;
-}
-
-template< class T >
-bool LCIter< T >::operator!=(LCIter< T > rhs) const {
-	return !(rhs == *this);
-}
-
-template< class T >
-const T& LCIter< T >::operator*() const {
-	assert(node != nullptr);
-	return node->val;
-}
-template< class T >
-const T* LCIter< T >::operator->() const {
-	assert(node != nullptr);
-	return std::addressof(node->val);
-}
-
+#endif
